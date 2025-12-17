703896b20fd286abdaa7e1e7caf4c6e3a25c486d
Tekraj15 <tekrajawasthi15@gmail.com>
2025-12-17 01:54:34 +0100

    Model constants like special tokens, image mean or std

---
+++ src/utils/constants.py
@@ -0,0 +1,12 @@
+# Model Constants
+IGNORE_INDEX = -100
+IMAGE_TOKEN_INDEX = -200
+DEFAULT_IMAGE_TOKEN = "<image>"
+DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
+DEFAULT_IM_START_TOKEN = "<im_start>"
+DEFAULT_IM_END_TOKEN = "<im_end>"
+
+# Mean and Std for SigLIP
+# SigLIP usually handles this in the processor, but good to have if manual.
+OPENAI_DATASET_MEAN = (0.48145466, 0.4578275, 0.40821073)
+OPENAI_DATASET_STD = (0.26862954, 0.26130258, 0.27577711)
